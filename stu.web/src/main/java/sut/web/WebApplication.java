4d4155b86ee42fb4eadd09b38b6a93ff54ded327
yuhua <yuhua_1987113@163.com>
2018-12-02 10:07:02 +0800

    'stu'

---
+++ stu.web/src/main/java/sut/web/WebApplication.java
@@ -0,0 +1,21 @@
+/**
+ * @author yuhua
+ * @date 2018/11/29 23:37
+ */
+
+package sut.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
+
+
+
+
+
